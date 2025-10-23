80c336680ac850ae9c86e0eb97a4fc270a92da7e
Alks丶 <2636777314@qq.com>
2025-10-23 11:30:30 +0800

    feat(notification): add JWT authentication support to notification module

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { NotificationGateway } from './notification.gateway';
 import { NotificationService } from './notification.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET', 'dev_secret_change_me'),
+        signOptions: { expiresIn: 604800 },
+      }),
+    }),
+  ],
   providers: [NotificationGateway, NotificationService],
   exports: [NotificationService],
 })
